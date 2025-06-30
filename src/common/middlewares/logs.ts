450c0f46d4143d9da2e11cb51b945f3d3ca76c4a
HagayAshkenazi <xd.HAshkenazi89@idf.il>
2025-06-30 14:13:46 +0300

    Fixhot: fix that latency is not content length

--- src/common/middlewares/logs.ts
+++ src/common/middlewares/logs.ts
@@ -3,11 +3,14 @@ import { logger } from '../helpers/logs';
 import type { Request, Response } from 'express';
 
 export const morganLogger = morgan((tokens: morgan.TokenIndexer, req: Request, res: Response) => {
+  const responseTime = Number(tokens['response-time'](req, res) ?? 0);
+  const contentLength = Number(tokens.res(req, res, 'content-length') ?? 0);
+
   logger.http('http request', {
     route: tokens.url(req, res) ?? '',
     method: tokens.method(req, res) ?? '',
     status: Number(tokens.status(req, res) ?? 0),
-    responseTime: Number(tokens['response-time'](req, res) ?? 0),
-    latency: Number(tokens.res(req, res, 'content-length') ?? 0),
+    responseTime,       
+    contentLength,     
   });
 });

6d721b019b73964ec154babe00ebb943c017dd44
HagayAshkenazi <xd.HAshkenazi89@idf.il>
2025-06-30 09:05:47 +0300

    Fix: Reomev from eslint.config file the "@generated/*", "src/common/interfaces/graphql/*"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default tsEslint.config(
   ...tsEslint.configs.recommended,
   prettierEslintRecommended,
   {
-    ignores: ["@generated/*", "src/common/interfaces/graphql/*", "dist/*"],
+    ignores: ["dist/*"],
   },
   {
     plugins: {

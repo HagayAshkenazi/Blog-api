16373645737ccfaa0b19be9a977ec30af6121b39
HagayAshkenazi <xd.HAshkenazi89@idf.il>
2025-07-03 15:48:22 +0300

    Add: add pgcrypto to the spell words

--- database/prisma/migrations/20250624171532_posts/migration.sql
+++ database/prisma/migrations/20250624171532_posts/migration.sql
@@ -10,10 +10,11 @@ CREATE TABLE IF NOT EXISTS blog.posts (
     CONSTRAINT posts_title_check CHECK (
         char_length(title) <= 25 AND 
         title !~* '\m(תשוש|תרנגול|פעמון|נועה|קירל|מרגול|קונץ)\M'
-    ),
+   ),
 
     CONSTRAINT posts_content_check CHECK (
         char_length(content) <= 200 AND 
         (length(regexp_replace(content, '[^א-ת]', '', 'g'))::double precision / GREATEST(length(content), 1)) > 0.5
     )
 );
+ 
